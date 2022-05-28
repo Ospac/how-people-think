ce72605114a86911692e7c52bf426de45d66277c
Seoharin <72519646+Seoharin@users.noreply.github.com>
2022-05-28 14:42:57 +0900

    Update db.py

--- db.py
+++ db.py
@@ -25,31 +25,59 @@ class DB:
         ts = self.cursor.fetchall()
         
         return str(ts[0][0])[:16]
-    
+    def execute(self,sql):
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except Exception as e :
+            print(" insert DB ",e)
+
     def get_id(self,table):
         self.cursor.execute('select count(*) from {table}'.format(table=table))
         num_data = self.cursor.fetchall()
         return num_data[0][0]+1
 
-    def insertDB(self,table, data):
-        
-            if table == 'history':
-                sql = " INSERT INTO {table} VALUES ({id},\'{ts}\',\'{topic}\',{prob})".format(table=table,id=data[0],ts=data[1],topic=data[2],prob=data[3])
-            try:
-                self.cursor.execute(sql)
-                self.db.commit()
-            except Exception as e :
-                print(" insert DB ",e)
+    def insertDB(self,data,keywords):
+            # step1 : insert data into 'HISTORY' table
+            sql = " INSERT INTO HISTORY VALUES ({id},\'{ts}\',\'{topic}\',{prob})".format(id=data['id'],ts=data['ts'],topic=data['topic'],prob=data['prob'])
+            self.execute(sql)
+            
+            # step2 : insert data into 'keywords' table
+            for key in keywords:
+                for word in keywords[key]:
+                    sql = "INSERT INTO KEYWORDS VALUES ({id},\'{type}\',\'{word}\')".format(id=data['id'],type=key,word=word)
+                    self.execute(sql)
 
-    def readDB(self,table,topic):
-        temp = []
-        if table == 'history':
-            sql = " SELECT ts, prob from {table} where topic = \'{topic}\'".format(table=table,topic=topic)
-        try:
-            self.cursor.execute(sql)
-            result = self.cursor.fetchall()
-        except Exception as e :
-            result = (" read DB err",e)
-        temp.append(result[0][0])
-        temp.append(result[0][1])
-        return temp
+    def get_history(self,topic):
+        ts = []
+        prob = []
+        sql = " SELECT ts, prob from history where topic = \'{topic}\'".format(topic=topic)
+        self.execute(sql)
+        result = self.cursor.fetchall()
+        print(result)
+        for i in result:
+            ts.append(i[0])
+            prob.append(i[1])
+        dict={'ts':ts, 'prob':prob}
+        return dict
+
+    def get_keywords(self,topic, ts):
+        pos=[]
+        neg=[]
+        neu=[]
+        self.cursor.execute(" SELECT id from history where topic = \'{topic}\' and ts= \'{ts}\'".format(topic=topic, ts = ts))
+        result = self.cursor.fetchall()
+        id = result[0][0]
+
+        sql = " SELECT type, word from keywords where id = \'{id}\'".format(id=id)
+        self.execute(sql)
+        result = self.cursor.fetchall()
+        for i in result:
+            if i[0]=='positive':
+                pos.append(i[1])
+            elif i[0]=='negative':
+                neg.append(i[1])
+            else:
+                neu.append(i[1])
+        dict={'positive':pos,'negative':neg,'neutral':neu}
+        return dict
