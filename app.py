69840b6d204d4c9bafd1920225f8e7d27da806d0
Ageha <ospac111@gmail.com>
2022-05-31 17:35:10 +0900

    Update api routes in app.js

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_restful import Api
 from routes.Index import Index
-from api.SearchResult import SearchResult
+from routes.Result import Result
 
 app = Flask(__name__)
 api = Api(app)
 
 #routes
 api.add_resource(Index, '/')
-api.add_resource(SearchResult, '/api/search')
+api.add_resource(Result, '/result')
 
 #API Server
 if __name__ == '__main__':
