7a1d04ac7b519c4923739a01b89316fcf19b84ae
Ageha <ospac111@gmail.com>
2022-05-31 17:42:53 +0900

    Update Index.py

--- routes/Index.py
+++ routes/Index.py
@@ -1,6 +1,5 @@
 from flask import make_response, render_template
 from flask_restful import Resource
-from controller.CrawledDataHandler import CrawledDataHandler
 
 class Index(Resource):
     def get(self):
