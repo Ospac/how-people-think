46568edd9aeb5754791481d019463c6493a6f5d8
Ageha <ospac111@gmail.com>
2022-05-31 17:29:03 +0900

    Update CrawledDataHandler.py

--- controller/CrawledDataHandler.py
+++ controller/CrawledDataHandler.py
@@ -2,25 +2,21 @@ import json
 from controller.DetectLanguage import DetectLanguage
 from controller.SentimentReq import SentimentReq
 
-def CrawledDataHandler():
+def CrawledDataHandler(searchInput): #searchInput을 crawling function으로 전달
     #Get CrawledData
     with open("test.json", "r") as crawledJson:
         crawledData = json.load(crawledJson)
     if len(crawledData["document"]) >= 5120:
         return "5120자를 초과하였습니다." , 400
 
-    #DetectLanguage (검색어만 detect or 전체문서 detect)
+    #DetectLanguage (검색어만 detect)
     detectedlanguage = DetectLanguage(crawledData["search_word"])
     data = {
         "language"  :   detectedlanguage,
         "text"      :   crawledData["document"]
     }
-    
     #DetectSentiment
     sentimentData = SentimentReq(data)
-    sentiment = sentimentData[0]['sentiment']
-    confidenceScores = sentimentData[0]['confidenceScores']
-    print(sentiment)
-    print(confidenceScores)    
+    return sentimentData[0]
 
 
