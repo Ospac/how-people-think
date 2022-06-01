277e6a0b00945e34e3dba87bcc58331735c2761e
JaeyoungAhn <quaki@naver.com>
2022-06-01 11:12:19 +0900

    modified

--- controller/CrawledDataHandler.py
+++ controller/CrawledDataHandler.py
@@ -4,16 +4,17 @@ from controller.SentimentReq import SentimentReq
 
 def CrawledDataHandler(searchInput): #searchInput을 crawling function으로 전달
     #Get CrawledData
-    with open("test.json", "r") as crawledJson:
-        crawledData = json.load(crawledJson)
-    if len(crawledData["document"]) >= 5120:
+	#with open("test.json", "r") as crawledJson:
+		#crawledData = json.load(crawledJson)
+	crawledData = TwitterCrawler(searchInput)
+    if len(crawledData[searchInput]) >= 5120:
         return "5120자를 초과하였습니다." , 400
 
     #DetectLanguage (검색어만 detect)
-    detectedlanguage = DetectLanguage(crawledData["search_word"])
+    detectedlanguage = DetectLanguage(crawledData[searchInput])
     data = {
         "language"  :   detectedlanguage,
-        "text"      :   crawledData["document"]
+        "text"      :   crawledData[searchInput]
     }
     #DetectSentiment
     sentimentData = SentimentReq(data)
