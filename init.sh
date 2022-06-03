c70d3e3ba88d8d57b6d2d8cc0deba7c31f1fa127
quaki <quaki@DESKTOP-R5G0O6M.localdomain>
2022-06-03 18:16:13 +0900

    compressed .env

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get install -y python3
 sudo apt-get install -y python-pip
+sudo apt-get install -y zip
+sudo apt-get install -y unzip
 
 python3 -m venv .venv
 source .venv/bin/activate
@@ -23,7 +25,20 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 cd ${PATH_NAME}
 echo "moved to ${PATH_NAME}"
 
+echo "Please enter the Password"
+read p
+while :
+do
+	unzip -P ${p} env.zip
+	if [ $? -eq 0 ]; then
+		echo "You entered correct password!"
+		break
+	else
+		echo "You entered incorrect password!"
+	fi
+done
+
 echo "Done."
 echo ""
 google-chrome --version
-echo "Download the same version of chromedriver as googole-chrome"
+echo "Make sure the current version is 102. If not, please manually download later version of chromedriver."
