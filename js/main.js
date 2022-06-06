2fe57f8ee8c1f9f1e7580f02be997f4fbc79a7cd
HEEJAEKKK <kyaid8804@knu.ac.kr>
2022-06-06 17:16:17 +0900

    front edit

--- js/main.js
+++ js/main.js
@@ -4,12 +4,27 @@ let barChart = new Chart(ChartOne, {
 
   type : 'doughnut', // pie, line, doughnut, polarArea
   data : {
-    labels : ['positive', 'negative', 'haha'],
+    labels : ['positive', 'negative', 'neutral'],
     datasets : [
       {
         label : 'peoples think',
         data:[10, 20, 30],
-        backgroundColor:['red', 'blue']
+        backgroundColor: [
+  					'#251D3A',
+  					'#2A2550',
+  					'#E04D01',
+  				],
+  			
     }]
-}
-})
+},
+options: {
+			responsive: false,
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true
+					}
+				}]
+			},
+		}
+});
