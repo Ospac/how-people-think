084e684d47752457db0aad2bef659475cce164d0
Ageha <ospac111@gmail.com>
2022-06-07 17:48:10 +0900

    Update for wordcloud

--- controller/CrawledDataHandler.py
+++ controller/CrawledDataHandler.py
@@ -1,24 +1,40 @@
-import json
 from controller.DetectLanguage import DetectLanguage
+from controller.KeyPhrases import KeyPhrases
 from controller.SentimentReq import SentimentReq
 from controller.TwitterCrawler import TwitterCrawler
-
-def CrawledDataHandler(searchInput): #searchInput을 crawling function으로 전달
-    #Get CrawledData
-	#with open("test.json", "r") as crawledJson:
-	    #crawledData = json.load(crawledJson)
+from controller.MakeWordCloud import MakeWordCloud
+# from db import DB
+def CrawledDataHandler(searchInput): 
     crawledData = TwitterCrawler(searchInput)
     if len(crawledData[searchInput]) >= 5120:
-        return "5120자를 초과하였습니다." , 400
+        return '5120자를 초과하였습니다.' , 400
 
-    #DetectLanguage (검색어만 detect)
     detectedlanguage = DetectLanguage(crawledData[searchInput])
     data = {
-        "language"  :   detectedlanguage,
-        "text"      :   crawledData[searchInput]
+        'language'  :   detectedlanguage,
+        'text'      :   crawledData[searchInput]
     }
-    #DetectSentiment
-    sentimentData = SentimentReq(data)
-    return sentimentData[0]
-
-
+    sentiment = SentimentReq(data)
+    totalLength = len(data["text"])
+    splitedLength = int(totalLength / 10) + 1
+    splitedData = [ data["text"][i : i + splitedLength] for i in range(0, totalLength, splitedLength)]
+    keyPhrase = KeyPhrases({'language' : data['language'], 'text' : splitedData})
+    keywordList = []
+    for data in keyPhrase:
+        for key in data['keyPhrases']:
+           keywordList.append(key)
+    imgPath = MakeWordCloud(keywordList)
+    # sentimentDict = {
+    #     'ts' : DB.get_timestamp(),
+    #     'topic' : searchInput,
+    #     'prob'  : sentiment['sentimentData'][0]['confidenceScores']['positive']
+    # }
+    # keywordDict = {
+    #     'keyword' : keyPhrase,
+    # }
+    resultData = {
+        "sentiment" : sentiment,
+        "searchInput" : searchInput,
+        "imgPath" : imgPath
+    }
+    return resultData
