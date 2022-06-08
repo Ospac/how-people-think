2f2db619a085cdcc27b6bdbee9406cf3df6ffd4c
JaeyoungAhn <39547923+JaeyoungAhn@users.noreply.github.com>
2022-06-08 19:01:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Api
 from routes.Index import Index
 from routes.Result import Result
+from controller.TrendCrawler import TrendCrawler
+#from controller.ThreadingStart import InitThreading
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +11,11 @@ api = Api(app)
 #routes
 api.add_resource(Index, '/')
 api.add_resource(Result, '/result')
+
+#automatic crawling with interval
+index = 0
+trendinglist = []
+ThreadingStart()
 
 #API Server
 if __name__ == '__main__':
