6d1c434507930650f3301006f01976faa098153e
Ageha <ospac111@gmail.com>
2022-06-08 17:24:00 +0900

    Update MakeWordCloud.py

--- controller/MakeWordCloud.py
+++ controller/MakeWordCloud.py
@@ -4,14 +4,15 @@ import uuid
 import matplotlib.pyplot as plt
 def MakeWordCloud(wordList):
     counts = Counter(wordList)
-    tags = counts.most_common(60)
+    tags = counts.most_common(40)
     font_path = "static/assets/fonts/NotoSansKR-Medium.otf"
     wc = WordCloud(
         font_path = font_path,
         max_font_size=60, 
         background_color="white" 
     )
-    cloud = wc.generate_from_frequencies(dict(tags))    
+    tagsDict = dict(tags)
+    cloud = wc.generate_from_frequencies(tagsDict)    
     img_path = 'static/assets/img/' + str(uuid.uuid4()) + '.png'
     cloud.to_file(img_path)
-    return img_path
+    return {"imgPath" : img_path, "tags" : tagsDict }
