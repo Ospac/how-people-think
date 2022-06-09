a05a1c91104846b7d20b849a124ce82db8637d3d
JaeyoungAhn <39547923+JaeyoungAhn@users.noreply.github.com>
2022-06-10 04:19:21 +0900

    update for recent searches

--- routes/Index.py
+++ routes/Index.py
@@ -1,6 +1,12 @@
 from flask import make_response, render_template
 from flask_restful import Resource
-
+from db import DB
+db=DB()
+getwords = db.get_words()
+tranwords = []
+for i in range(len(getwords)):
+    tranwords.extend(getwords[i])
+    tranwords[i].split("(',)")
 class Index(Resource):
     def get(self):
-        return make_response(render_template('index.html'))
+        return make_response(render_template('index.html', words = tranwords))
